dd2fd7011f2712433a0a004327779167827c1fa6
Javier Alcaraz <jalcaraz@live.com>
2023-12-13 21:25:49 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { useState } from 'react'
+import { puppyList } from './data.js'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
 
   return (
-    <>
-      <div>
-       
-      </div>
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
 export default App
